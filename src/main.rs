930a06feca2efb4e9771242468098e67a3da0a84
MetaphorC <ypstomar57@gmail.com>
2025-05-01 21:38:45 +0530

    kinda usable now

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use ncurses::*;
 use std::fs::File;
-use std::io::{self, Write, BufRead, BufReader};
+use std::io::{self, Write, ErrorKind, BufRead};
 use std::env;
 use std::process;
 use std::ops::{Add, Mul};
 use std::cmp;
 
+mod ctrlc;
 
 const REGULAR_PAIR: i16 = 0;
 const HIGHLIGHT_PAIR: i16 = 1;
@@ -59,8 +60,8 @@ impl Layout {
     fn available_pos(&self) -> Vec2 {
         use LayoutKind::*;
         match self.kind {
-            Vert => self.pos + self.size * Vec2::new(1, 0),
-            Horz => self.pos + self.size * Vec2::new(0, 1),
+            Horz => self.pos + self.size * Vec2::new(1, 0),
+            Vert => self.pos + self.size * Vec2::new(0, 1),
         }
     }
 
@@ -81,7 +82,8 @@ impl Layout {
 
 #[derive(Default)]
 struct Ui {
-    layouts: Vec<Layout>
+    layouts: Vec<Layout>,
+    key: Option<i32>,
 }
 
 impl Ui {
@@ -95,7 +97,9 @@ impl Ui {
     }
 
     fn begin_layout(&mut self, kind: LayoutKind) {
-        let layout = self.layouts.last()
+        let layout = self
+            .layouts
+            .last()
             .expect("Can't create a layout outside of Ui::begin() and Ui::end()");
         let pos = layout.available_pos();
         self.layouts.push(Layout {
@@ -131,6 +135,74 @@ impl Ui {
         layout.add_widget(Vec2::new(width, 1));
     }
 
+    fn edit_field(&mut self, buffer:&mut String, cursor: &mut usize, width: i32) {
+        let layout = self
+            .layouts
+            .last_mut()
+            .expect("Trying to render edit field outside of any layout");
+        let pos = layout.available_pos();
+
+        if *cursor > buffer.len() {
+            *cursor = buffer.len();
+        }
+
+        if let Some(key) = self.key.take() {
+            match key {
+                32..=126 => {
+                    if *cursor >= buffer.len() {
+                        buffer.push(key as u8 as char);
+                    } else {
+                        buffer.insert(*cursor, key as u8 as char);
+                    }
+                    *cursor += 1;
+                }
+                constants::KEY_LEFT => {
+                    if *cursor > 0 {
+                        *cursor -= 1
+                    }
+                }
+                constants::KEY_RIGHT => {
+                    if *cursor < buffer.len() {
+                        *cursor += 1;
+                    }
+                }
+                constants::KEY_BACKSPACE => {
+                    if *cursor > 0 {
+                        *cursor -= 1;
+                        if *cursor < buffer.len() {
+                            buffer.remove(*cursor);
+                        }
+                    }
+                }
+                constants::KEY_DC => {
+                    if *cursor < buffer.len() {
+                        buffer.remove(*cursor);
+                    }
+                }
+                _ => {
+                    self.key = Some(key);
+                }
+            }
+        }
+
+        // BUFFER
+        {
+            mv (pos.y, pos.x);
+            attron(COLOR_PAIR(REGULAR_PAIR));
+            addstr(buffer);
+            attroff(COLOR_PAIR(REGULAR_PAIR));
+            layout.add_widget(Vec2::new(width, 1));
+        }
+
+        // CURSOR
+        {
+            mv (pos.y, pos.x);
+            attron(COLOR_PAIR(HIGHLIGHT_PAIR));
+            addstr(buffer.get(*cursor..=*cursor).unwrap_or(" "));
+            attroff(COLOR_PAIR(HIGHLIGHT_PAIR));
+        }
+    }
+    
     #[allow(dead_code)]
     fn label(&mut self, text: &str, pair: i16) {
         self.label_fixed_width(text, text.len() as i32, pair);
@@ -159,10 +231,12 @@ impl Status {
 }
 
 fn parse_item(line: &str) -> Option<(Status, &str)> {
-    let todo_item =
-        line.strip_prefix("TODO: ").map(|title| (Status::Todo, title));
-    let done_item =
-        line.strip_prefix("DONE: ").map(|title| (Status::Done, title));
+    let todo_item = line
+        .strip_prefix("TODO: ")
+        .map(|title| (Status::Todo, title));
+    let done_item = line
+        .strip_prefix("DONE: ")
+        .map(|title| (Status::Done, title));
     todo_item.or(done_item)
 }
 
@@ -192,6 +266,18 @@ fn list_down(list: &[String], list_curr: &mut usize) {
     }
 }
 
+fn list_first(list_curr: &mut usize) {
+    if *list_curr > 0 { 
+        *list_curr = 0;
+    }
+}
+
+fn list_last(list: &[String], list_curr: &mut usize) {
+    if !list.is_empty() {
+        *list_curr = list.len() - 1;
+    }
+}
+
 fn list_transfer(
     list_dst: &mut Vec<String>,
     list_src: &mut Vec<String>,
@@ -205,17 +291,28 @@ fn list_transfer(
     }
 }
 
-fn load_state(todos: &mut Vec<String>, dones: &mut  Vec<String>, file_path: &str ) {
+fn list_delete(list: &mut Vec<String>, list_curr: &mut usize) {
+    if *list_curr < list.len() {
+        list.remove(*list_curr);
+        if *list_curr >= list.len() && !list.is_empty() {
+            *list_curr = list.len() - 1;
+        }
+    }
+}
+
+fn load_state(todos: &mut Vec<String>, dones: &mut  Vec<String>, file_path: &str ) -> io::Result<()> {
     let file = File::open(file_path).unwrap();
-    for (index, line) in BufReader::new(file).lines().enumerate() {
-        match parse_item(&line.unwrap()) {
+    for (index, line) in io::BufReader::new(file).lines().enumerate() {
+        match parse_item(&line?) {
             Some((Status::Todo, title)) => todos.push(title.to_string()),
             Some((Status::Done, title)) => dones.push(title.to_string()),
             None => {
                 eprintln!("{}:{}: ERROR: ill-formed item line", file_path, index + 1);
+                process::exit(1);
             }
         }
     }
+    Ok(())
 }
 
 fn save_state(todos: &[String], dones: &[String], file_path: &str) {
@@ -238,6 +335,8 @@ fn save_state(todos: &[String], dones: &[String], file_path: &str) {
 
 
 fn main() {
+    ctrlc::init();
+
     let mut args = env::args();
     args.next().unwrap();
 
@@ -255,10 +354,26 @@ fn main() {
     let mut dones = Vec::<String>::new();
     let mut done_curr: usize = 0;
 
-    load_state(&mut todos, &mut dones, &file_path);
+    let mut notification: String;
+
+    match load_state(&mut todos, &mut dones, &file_path) {
+        Ok(()) => notification = format!("Loaded file {}", file_path),
+        Err(error) => {
+            if error.kind() == ErrorKind::NotFound {
+                notification = format!("New file {}", file_path)
+            } else {
+                panic!(
+                    "Could not load state from file `{}`: {:?}",
+                    file_path, error
+                )
+            }
+        }
+    };
 
     initscr();
     noecho();
+    keypad(stdscr(), true);
+    timeout(16);
     curs_set(CURSOR_VISIBILITY::CURSOR_INVISIBLE);
 
     start_color();
@@ -267,100 +382,185 @@ fn main() {
 
     let mut quit = false;
     let mut panel = Status::Todo;
+    let mut editing = false;
+    let mut editing_cursor = 0;
 
     let mut ui = Ui::default();
-    while !quit {
+    while !quit && !ctrlc::poll() {
         erase();
 
         let mut x = 0;
         let mut y = 0;
         getmaxyx(stdscr(), &mut y, &mut x);
 
-        ui.begin(Vec2::new(0, 0), LayoutKind::Horz);
+        ui.begin(Vec2::new(0, 0), LayoutKind::Vert);
         {
-            ui.begin_layout(LayoutKind::Vert);
-            {
-                ui.label_fixed_width(
-                    "TODO",
-                    x / 2,
-                    if panel == Status::Todo {
-                        HIGHLIGHT_PAIR
-                    } else {
-                        REGULAR_PAIR
-                    });
-                for (index, todo) in todos.iter().enumerate() {
-                    ui.label_fixed_width(
-                        &format!("- [ ] {}", todo),
-                        x / 2,
-                    if index == todo_curr && panel == Status::Todo {
-                        HIGHLIGHT_PAIR
-                    } else {
-                        REGULAR_PAIR
-                    });
-                }
-            }
-            ui.end_layout();
+            ui.label_fixed_width(&notification, x, REGULAR_PAIR);
+            ui.label_fixed_width("", x, REGULAR_PAIR);
 
-            ui.begin_layout(LayoutKind::Vert);
+            ui.begin_layout(LayoutKind::Horz);
             {
-                ui.label_fixed_width(
-                    "DONE",
-                    x / 2,
-                    if panel == Status::Done {
-                        HIGHLIGHT_PAIR
-                    } else {
-                        REGULAR_PAIR
-                    });
-                for (index, done) in dones.iter().enumerate() {
-                    ui.label_fixed_width(
-                        &format!("- [x] {}", done),
-                        x / 2,
-                        if index == done_curr && panel == Status::Done {
-                            HIGHLIGHT_PAIR
-                        } else {
-                            REGULAR_PAIR
-                        });
+                ui.begin_layout(LayoutKind::Vert);
+                {
+                    if panel == Status::Todo {
+                        ui.label_fixed_width("TODO", x/2, HIGHLIGHT_PAIR);
+                        for(index, todo) in todos.iter_mut().enumerate() {
+                            if index == todo_curr {
+                                if editing {
+                                    ui.edit_field(todo, &mut editing_cursor, x / 2);
+
+                                    if let Some('\n') = ui.key.take().map(|x| x as u8 as char) {
+                                        editing = false;
+                                    }
+                                } else {
+                                    ui.label_fixed_width(
+                                        &format!("- [ ] {}", todo),
+                                        x / 2,
+                                        HIGHLIGHT_PAIR,
+                                    );
+                                    if let Some('r') = ui.key.map(|x| x as u8 as char) {
+                                        editing = true;
+                                        editing_cursor = todo.len();
+                                        ui.key = None;
+                                    }
+                                }
+                            } else {
+                                ui.label_fixed_width(
+                                    &format!("- [ ] {}", todo),
+                                    x / 2,
+                                    REGULAR_PAIR,
+                                );
+                            }
+                        }
+
+                        if let Some(key) = ui.key.take() {
+                            match key as u8 as char { 
+                                'K' => list_drag_up(&mut todos, &mut todo_curr),
+                                'J' => list_drag_down(&mut todos, &mut todo_curr),
+                                'i' => {
+                                    todos.insert(todo_curr, String::new());
+                                    editing_cursor = 0;
+                                    editing = true;
+                                    notification.push_str("What needs to be done?");
+                                }
+                                'd' => {
+                                    notification.push_str(
+                                        "Can't remove items from TODO. Mark it as DONE first.",
+                                    );
+                                }
+                                'k' => list_up(&mut todo_curr),
+                                'j' => list_down(&todos, &mut todo_curr),
+                                'g' => list_first(&mut todo_curr),
+                                'G' => list_last(&mut todos, &mut todo_curr),
+                                '\n' => {
+                                    list_transfer(&mut dones, &mut todos, &mut todo_curr);
+                                    notification.push_str("DONE!")
+                                }
+                                '\t' => {
+                                    panel = panel.toggle();
+                                }
+                                _ => {
+                                    ui.key = Some (key);
+                                }
+                            }
+                        }
+                    } else { 
+                        ui.label_fixed_width("TODO", x / 2, REGULAR_PAIR);
+                        for todo in todos.iter() {
+                            ui.label_fixed_width(&format!("- [ ] {}", todo), x / 2, REGULAR_PAIR);
+                        }
+                    }
                 }
+                ui.end_layout();
+
+                ui.begin_layout(LayoutKind::Vert);
+                {
+                    if panel == Status::Done {
+                        ui.label_fixed_width("DONE", x/2, HIGHLIGHT_PAIR);
+                        for(index, done) in dones.iter_mut().enumerate() {
+                            if index == done_curr {
+                                if editing {
+                                    ui.edit_field(done, &mut editing_cursor, x / 2);
+
+                                    if let Some('\n') = ui.key.take().map(|x| x as u8 as char) {
+                                        editing = false;
+                                    }
+                                } else {
+                                    ui.label_fixed_width(
+                                        &format!("- [ ] {}", done),
+                                        x / 2,
+                                        HIGHLIGHT_PAIR,
+                                    );
+                                    if let Some('r') = ui.key.map(|x| x as u8 as char) {
+                                        editing = true;
+                                        editing_cursor = done.len();
+                                        ui.key = None;
+                                    }
+                                }
+                            } else {
+                                ui.label_fixed_width(
+                                    &format!("- [ ] {}", done),
+                                    x / 2,
+                                    REGULAR_PAIR,
+                                );
+                            }
+                        }
+
+                        if let Some(key) = ui.key.take() {
+                            match key as u8 as char { 
+                                'K' => list_drag_up(&mut dones, &mut done_curr),
+                                'J' => list_drag_down(&mut dones, &mut done_curr),
+                                'i' => {
+                                    notification.push_str(
+                                        "Can't insert new DONE items, only DONE allowed."
+                                    );
+                                }
+                                'd' => {
+                                    list_delete(&mut dones, &mut done_curr);
+                                    notification.push_str("BURN TO ASHES!");
+                                }
+                                'k' => list_up(&mut done_curr),
+                                'j' => list_down(&dones, &mut done_curr),
+                                'g' => list_first(&mut done_curr),
+                                'G' => list_last(&mut dones, &mut done_curr),
+                                '\n' => {
+                                    list_transfer(&mut todos, &mut dones, &mut done_curr);
+                                    notification.push_str("No, not done yet...")
+                                }
+                                '\t' => {
+                                    panel = panel.toggle();
+                                }
+                                _ => ui.key = Some (key),
+                            }
+                        }
+                    } else { 
+                        ui.label_fixed_width("DONE", x / 2, REGULAR_PAIR);
+                        for done in dones.iter() {
+                            ui.label_fixed_width(&format!("- [ ] {}", done), x / 2, REGULAR_PAIR);
+                        }
+                    }
+                }
+                ui.end_layout();
             }
             ui.end_layout();
         }
         ui.end();
 
+        if let Some('q') = ui.key.take().map(|x| x as u8 as char) {
+            quit = true;
+        }
+
         refresh();
 
         let key = getch();
-        match key as u8 as char {
-            'q' => quit = true,
-            'W' => match panel {
-                Status::Todo => list_drag_up(&mut todos, &mut todo_curr),
-                Status::Done => list_drag_up(&mut dones, &mut done_curr),
-            }
-            'S' => match panel {
-                Status::Todo => list_drag_down(&mut todos, &mut todo_curr),
-                Status::Done => list_drag_down(&mut dones, &mut done_curr),
-            }
-            'w' => match panel {
-                Status::Todo => list_up(&mut todo_curr),
-                Status::Done => list_up(&mut done_curr)
-            },
-            's' => match panel {
-                Status::Todo => list_down(&todos, &mut todo_curr),
-                Status::Done => list_down(&dones, &mut done_curr)
-            },
-            '\n' => match panel {
-                Status::Todo => list_transfer(&mut dones, &mut todos, &mut todo_curr),
-                Status::Done => list_transfer(&mut todos, &mut dones, &mut done_curr),
-            },
-            '\t' => {
-                panel = panel.toggle();
-            }
-            _ => {
-               // todos.push(format!("{}", key));
-            }
+        if key != ERR {
+            notification.clear();
+            ui.key = Some(key);
         }
     }
 
-    save_state(&todos, &dones, &file_path);
-   
     endwin();
+
+    save_state(&todos, &dones, &file_path);
+    println!("Saved state to {}", file_path);
 }
